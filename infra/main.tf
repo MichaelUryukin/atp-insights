f791d40d46aeae7d67bb2527df9c61e48c8ac52e
Alla Bongard <alla.bongard@gmail.com>
2025-12-15 19:00:45 +0100

    removed form infra

--- infra/main.tf
+++
@@ -1,9 +0,0 @@
-resource "snowflake_database" "atp_insights" {
-  name    = "ATP_INSIGHTS"
-}
-
-resource "snowflake_schema" "default" {
-  database = snowflake_database.atp_insights.name
-  name     = "DEFAULT"
-}
-
